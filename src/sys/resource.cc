e47e71b4f2b82d18a545182a040dc4524c392077
Michał Cłapiński <michal.clapinski@codilime.com>
2018-09-28 18:54:07 +0200

    Return -1 in posix stubs
    We were returning 0 in stub functions without setting anything
    in the passed structures. Therefore the results were dependent
    on random memory. Now we return -1, so nobody should use those
    structures.
    
    Change-Id: I7c767d17a98fd8c4c1ce7042194a9c373667f044
    Partial-Bug: #1737177

--- src/sys/resource.cc
+++ src/sys/resource.cc
@@ -1,13 +1,13 @@
 #include <sys/resource.h>
 
 int getrusage(int, struct rusage *) {
-    return 0; 
+    return -1;
 }
 
 int getrlimit(int, struct rlimit *) {
-    return 0; 
+    return -1;
 }
 
 int setrlimit(int, struct rlimit const *) {
-    return 0; 
+    return -1;
 }
